0e75cba395d9cb7b072c96265ea1dca4494c1ba9
Tamao <reneozbay3@gmail.com>
2024-11-19 20:04:08 +0100

    just in case

---
+++ landing.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>ZiZi Chat</title>
+<link rel="icon" href="Pictures/Z.png">
+
+<style>
+    html{
+            font-family: "Inter", Arial, Helvetica, sans-serif;
+            color: #ebebf4;
+            background-color: #05050f;
+}
+
+ul {
+    display: flex;
+    align-items: center;
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    border-bottom: solid 1px white;
+}
+
+a {
+    display: flex; 
+    align-items: center;
+    text-decoration: none;
+    color: white;
+}
+
+li {
+    margin-left: 20px; 
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  
+}
+
+li a:hover {
+    background-color: #0a0a1d;
+}
+
+.right-space {
+    margin-left: auto;
+    display: flex;
+    margin-right: 100px;
+}
+
+.z-icon {
+    width: 64px;
+    height: 64px;
+    margin-left: 100px;
+    margin-right: 15px;
+}
+
+        .box{
+        font-size: large;
+        width: 850px;
+        height: 750px;
+        border: solid 1px white;
+        border-radius: 10px;
+        text-align: center;
+        margin: 0 auto;
+        margin-top: 60px;
+        padding-top: 1px;
+        padding-bottom: 10px;
+    }
+
+    .box img{
+        width: 600px;
+        display: block;
+        margin: 35px auto;
+        border-radius: 15px;
+    }
+
+    .box a{
+    display: inline-block;
+    margin: 0 5px;
+    color: #ebebf4;
+    text-decoration: none;
+    font-size: 18px;
+    padding: 10px 10px; 
+    border: 1px solid white;
+    border-radius: 5px; 
+    }
+</style>
+</head>
+<body>
+<nav>
+    <ul>
+        <a href="google.com">
+            <img class="z-icon" src="Pictures/Z.png" alt="Z Icon"> Chat ZiZi
+        </a>
+        <div class="right-space">
+            <li><a class="active" href="#home">Home</a></li>
+            <li><a href="#about">Sign up</a></li>
+            <li><a href="#about">Sign in</a></li>
+        </div>
+    </ul>
+</nav>
+
+    <div class="box">
+        <h1>👋Welcome To ZiZi!</h1>
+        <p>Chat with anyone, anytime—securely and effortlessly.
+<br>
+Sign in or create an account to get started!</p>
+<br>
+<p> <a href="google.com">Sign In</a> or <a href="google.com">Create An Account</a> here.</p>
+    <img src="https://media.istockphoto.com/id/1217093906/photo/womens-hand-typing-on-mobile-smartphone-live-chat-chatting-on-application-communication.jpg?s=612x612&w=0&k=20&c=37apRN2rOKxtb2MCDxoIlmi5ImkDkGLwSxYDyC-Ljc0=">
+    </div>
+</body>
+</html>
--- signin.php
+++ signin.php
@@ -10,24 +10,6 @@
         box-sizing: border-box;
     }
 
-    [class*="col-"]{
-        float: left;
-        padding: 15px;
-    }
-
-    .col-1 {width: 8.33%;}
-    .col-2 {width: 16.66%;}
-    .col-3 {width: 25%;}
-    .col-4 {width: 33.33%;}
-    .col-5 {width: 41.66%;}
-    .col-6 {width: 50%;}
-    .col-7 {width: 58.33%;}
-    .col-8 {width: 66.66%;}
-    .col-9 {width: 75%;}
-    .col-10 {width: 83.33%;}
-    .col-11 {width: 91.66%;}
-    .col-12 {width: 100%;}
-
     html{
         font-family: "Inter", Arial, Helvetica, sans-serif;
     }
@@ -51,7 +33,7 @@
         border-radius: 10px;
         text-align: center;
         margin: 0 auto;
-        margin-top: 00px;
+        margin-top: 0px;
         padding-top: 1px;
         padding-bottom: 10px;
         box-shadow:
@@ -88,6 +70,13 @@
     .sign-up-box p{
         padding-top: 20px;
     }
+
+.cover{
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    width: 50%;
+}
     </style>
 </head>
 <body>
@@ -98,7 +87,7 @@
 
 
         
-    <div class="sign-up-box" class="col-2">
+    <div class="sign-up-box">
         <h2>Welcome</h2>
         <p>Username</p>
         <input type="text" placeholder="Username" required>
